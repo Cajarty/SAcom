03715115f40ac1b019c9c5f2c157d71836c2c9f6
Jaegar Sarauer <jaegars@hotmail.com>
2019-11-23 15:10:49 -0800

    Migrated to next.js

--- summary-answers/src/pages/index.js
+++ summary-answers/src/pages/index.js
@@ -52,11 +52,13 @@ class SearchBar extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form className='searchBarContainer' onSubmit={this.handleSubmit}>
         <label>
           Search for a Summary:
-          <input type="text" value={this.state.searchValue} onChange={this.handleChange} />
+          <br/>
+          <input className='searchBar' type="text" value={this.state.searchValue} onChange={this.handleChange} />
         </label>
+        <br/>
         <input type="submit" value="Submit" />
         <Results/>
       </form>
@@ -66,15 +68,7 @@ class SearchBar extends React.Component {
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-    
+    <SEO title="Summary Search Home" />
     <SearchBar/>
   </Layout>
 )
